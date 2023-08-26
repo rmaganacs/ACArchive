b27a305d139c9f3d0c00262385727fcbb98b0318
Ricardo Magana <rmaganacs@gmail.com>
2023-08-25 23:06:12 -0400

    Updated many features

--- app/src/main/java/com/example/acarchive/MainActivity.kt
+++ app/src/main/java/com/example/acarchive/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.acarchive
 
 import WebViewIntegration
+import android.app.Dialog
+import android.graphics.Color
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.webkit.WebView
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
@@ -16,8 +25,12 @@ import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
+    private var isAudioPlaying = true
+    private lateinit var mediaPlayer: MediaPlayer
     private var viewPager2: ViewPager2? = null
     private lateinit var webView: WebView
+    private lateinit var audioImage: ImageView
+    private lateinit var wikiButton: Button
     private lateinit var webViewIntegration: WebViewIntegration
     private var lastSelectedPosition: Int = 0
 
@@ -32,6 +45,11 @@ class MainActivity : AppCompatActivity() {
         )
 
         setupViewPager()
+
+        // Audio Setup
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
     }
 
     private fun setupViewPager() {
@@ -69,7 +87,40 @@ class MainActivity : AppCompatActivity() {
         }
         viewPager2?.setPageTransformer(compositePageTransformer)
         viewPager2?.setCurrentItem(lastSelectedPosition, false)
+
+        audioImage = findViewById(R.id.audio)
+        if(isAudioPlaying) {
+            audioImage.setImageResource(R.drawable.audio_on)
+        } else {
+            audioImage.setImageResource(R.drawable.audio_off)
+        }
+        audioImage.setOnClickListener {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+                isAudioPlaying = false
+                audioImage.setImageResource(R.drawable.audio_off)
+            } else {
+                mediaPlayer.start()
+                isAudioPlaying = true
+                audioImage.setImageResource(R.drawable.audio_on)
+            }
+        }
     }
+
+    private fun showOptionDialog(url: String, position: Int) {
+        val dialogView = layoutInflater.inflate(R.layout.modal_option, null)
+        val dialogBuilder = AlertDialog.Builder(this).setView(dialogView)
+        val alertDialog = dialogBuilder.create()
+
+        alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        alertDialog.show()
+        wikiButton = dialogView.findViewById(R.id.wikiButton)
+        wikiButton.setOnClickListener {
+            alertDialog.dismiss()
+            showWebView(url, position)
+        }
+    }
+
 
     private fun showWebView(url: String, position: Int) {
         lastSelectedPosition = position
@@ -104,24 +155,41 @@ class MainActivity : AppCompatActivity() {
 
     fun onSliderItemClick(position: Int) {
         when (position) {
-            0 -> showWebView("https://assassinscreed.fandom.com/wiki/Alexios", 0)
-            1 -> showWebView("https://assassinscreed.fandom.com/wiki/Kassandra", 1)
-            2 -> showWebView("https://assassinscreed.fandom.com/wiki/Bayek", 2)
-            3 -> showWebView("https://assassinscreed.fandom.com/wiki/Eivor_Varinsdottir", 3)
-            4 -> showWebView("https://assassinscreed.fandom.com/wiki/Alta%C3%AFr_Ibn-La%27Ahad", 4)
-            5 -> showWebView("https://assassinscreed.fandom.com/wiki/Ezio_Auditore_da_Firenze", 5)
-            6 -> showWebView("https://assassinscreed.fandom.com/wiki/Shao_Jun", 6)
-            7 -> showWebView("https://assassinscreed.fandom.com/wiki/Edward_Kenway", 7)
-            8 -> showWebView("https://assassinscreed.fandom.com/wiki/Shay_Cormac", 8)
-            9 -> showWebView("https://assassinscreed.fandom.com/wiki/Ratonhnhak%C3%A9:ton", 9)
-            10 -> showWebView("https://assassinscreed.fandom.com/wiki/Aveline_de_Grandpr%C3%A9", 10)
-            11 -> showWebView("https://assassinscreed.fandom.com/wiki/Arno_Dorian", 11)
-            12 -> showWebView("https://assassinscreed.fandom.com/wiki/Evie_Frye", 12)
-            13 -> showWebView("https://assassinscreed.fandom.com/wiki/Jacob_Frye", 13)
-            14 -> showWebView("https://assassinscreed.fandom.com/wiki/Desmond_Miles", 14)
+            0 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Alexios", 0)
+            1 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Kassandra", 1)
+            2 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Bayek", 2)
+            3 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Eivor_Varinsdottir", 3)
+            4 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Alta%C3%AFr_Ibn-La%27Ahad", 4)
+            5 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Ezio_Auditore_da_Firenze", 5)
+            6 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Shao_Jun", 6)
+            7 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Edward_Kenway", 7)
+            8 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Shay_Cormac", 8)
+            9 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Ratonhnhak%C3%A9:ton", 9)
+            10 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Aveline_de_Grandpr%C3%A9", 10)
+            11 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Arno_Dorian", 11)
+            12 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Evie_Frye", 12)
+            13 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Jacob_Frye", 13)
+            14 -> showOptionDialog("https://assassinscreed.fandom.com/wiki/Desmond_Miles", 14)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
         }
     }
 
 }
-
-
